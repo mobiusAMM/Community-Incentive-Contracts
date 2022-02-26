f1660a546030ed208b509ece6ecbf1b6d0211c81
Dylan Mooers <dylmooers@gmail.com>
2022-02-26 14:45:34 -0800

    initialize MobiusCommunityPayment contract

---
+++ contracts/MobiusCommunityPayment.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MobiusCommunityPayment {
+    enum BugLevels {
+        BASIC,
+        MEDIUM,
+        IMPORTANT
+    }
+}
