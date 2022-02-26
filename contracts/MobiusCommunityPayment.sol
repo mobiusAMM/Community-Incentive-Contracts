bd8a0e451271a39847e46e921be4a3e6cb2e6f6b
Dylan Mooers <dylmooers@gmail.com>
2022-02-26 14:52:02 -0800

    defined BugReport and GeneralRequest structs

--- contracts/MobiusCommunityPayment.sol
+++ contracts/MobiusCommunityPayment.sol
@@ -1,12 +1,29 @@
 pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MobiusCommunityPayment {
-    enum BugLevels {
+    enum BugLevel {
         BASIC,
         MEDIUM,
         IMPORTANT
+    }
+
+    struct BugReport {
+        uint256 id;
+        address origin;
+        BugLevel perceivedLevel;
+        bool fulfilled;
+        uint256 approvals;
+    }
+
+    struct GeneralRequest {
+        uint256 id;
+        address origin;
+        uint256 requestedReward;
+        bool fulfilled;
+        uint256 approvals;
     }
 }
